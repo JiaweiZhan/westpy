46fb48cad1af415adf67b951b336deaf5ecc4bbe
JiaweiZhan <jiaweiz@uchicago.edu>
2024-05-18 22:00:02 -0500

    multiprocesses write pdep

--- westpy/io_kernel/qe_io.py
+++ westpy/io_kernel/qe_io.py
@@ -1,7 +1,15 @@
+import os
+
+os.environ["OMP_NUM_THREADS"] = "1"  # For OpenMP
+os.environ["OPENBLAS_NUM_THREADS"] = "1"  # For OpenBLAS
+os.environ["MKL_NUM_THREADS"] = "1"  # For MKL
+
 import numpy as np
 import json
 import os, sys
 from typing import List
+import tempfile
+from multiprocessing import Pool
 from concurrent.futures import ThreadPoolExecutor
 
 from .base_io import base_io
@@ -12,10 +20,25 @@ HD_ID_VERSION = 0
 HD_ID_LITTLE_ENDIAN = 1
 HD_ID_DIMENSION = 2
 
+
+def write_pdep(pdep_val: np.ndarray, fname: str):
+    header = np.zeros(HD_LENGTH, dtype="int32")
+    header[HD_ID_VERSION] = HD_VERSION
+    header[HD_ID_DIMENSION] = pdep_val.size
+    header[HD_ID_LITTLE_ENDIAN] = 1 if sys.byteorder == "little" else 0
+    with open(fname, "wb") as f:
+        f.write(header.tobytes())
+        f.write(pdep_val.astype(np.complex128).tobytes())
+
+
+def write_wstat_helper(irow, fname, pdepvec_fname, outFolder):
+    pdepvec = np.load(pdepvec_fname, mmap_mode="r+")
+    vec = pdepvec[irow]
+    write_pdep(vec, os.path.join(outFolder, fname))
+
+
 class qe_io(base_io):
-    def __init__(self,
-                 wfc_fname=None,
-                 wstat_folder=None):
+    def __init__(self, wfc_fname=None, wstat_folder=None):
         super().__init__()
         self.wfc_name = wfc_fname
         self.wstat_folder = wstat_folder
@@ -32,57 +55,65 @@ class qe_io(base_io):
             else:
                 raise TypeError
 
-    def read_wfc(self,
-                 fname: str,
-                 read_evc: bool=True):
+    def read_wfc(self, fname: str, read_evc: bool = True):
         self.fname = fname
-        with open(self.fname, 'rb') as f:
+        with open(self.fname, "rb") as f:
             # Moves the cursor 4 bytes to the right
             f.seek(4)
 
-            self.ik = np.fromfile(f, dtype='int32', count=1)[0]
-            self.xk = np.fromfile(f, dtype='float64', count=3)
-            self.ispin = np.fromfile(f, dtype='int32', count=1)[0]
-            self.gamma_only = bool(np.fromfile(f, dtype='int32', count=1)[0])
-            self.scalef = np.fromfile(f, dtype='float64', count=1)[0]
+            self.ik = np.fromfile(f, dtype="int32", count=1)[0]
+            self.xk = np.fromfile(f, dtype="float64", count=3)
+            self.ispin = np.fromfile(f, dtype="int32", count=1)[0]
+            self.gamma_only = bool(np.fromfile(f, dtype="int32", count=1)[0])
+            self.scalef = np.fromfile(f, dtype="float64", count=1)[0]
 
             # Move the cursor 8 byte to the right
             f.seek(8, 1)
 
-            self.ngw = np.fromfile(f, dtype='int32', count=1)[0]
-            self.igwx = np.fromfile(f, dtype='int32', count=1)[0]
-            self.npol = np.fromfile(f, dtype='int32', count=1)[0]
-            self.nbnd = np.fromfile(f, dtype='int32', count=1)[0]
+            self.ngw = np.fromfile(f, dtype="int32", count=1)[0]
+            self.igwx = np.fromfile(f, dtype="int32", count=1)[0]
+            self.npol = np.fromfile(f, dtype="int32", count=1)[0]
+            self.nbnd = np.fromfile(f, dtype="int32", count=1)[0]
 
             # Move the cursor 8 byte to the right
             f.seek(8, 1)
 
-            self.b1 = np.fromfile(f, dtype='float64', count=3)
-            self.b2 = np.fromfile(f, dtype='float64', count=3)
-            self.b3 = np.fromfile(f, dtype='float64', count=3)
+            self.b1 = np.fromfile(f, dtype="float64", count=3)
+            self.b2 = np.fromfile(f, dtype="float64", count=3)
+            self.b3 = np.fromfile(f, dtype="float64", count=3)
 
-            f.seek(8,1)
-            
-            self.mill = np.fromfile(f, dtype='int32', count=3*self.igwx)
-            self.mill = self.mill.reshape( (self.igwx, 3) ) 
+            f.seek(8, 1)
+
+            self.mill = np.fromfile(f, dtype="int32", count=3 * self.igwx)
+            self.mill = self.mill.reshape((self.igwx, 3))
 
             if read_evc:
-                self.evc = np.zeros( (self.nbnd, self.npol * self.igwx), dtype="complex128")
+                self.evc = np.zeros(
+                    (self.nbnd, self.npol * self.igwx), dtype="complex128"
+                )
 
-                f.seek(8,1)
+                f.seek(8, 1)
                 for i in range(self.nbnd):
-                    self.evc[i,:] = np.fromfile(f, dtype='complex128', count=self.npol * self.igwx)
+                    self.evc[i, :] = np.fromfile(
+                        f, dtype="complex128", count=self.npol * self.igwx
+                    )
                     f.seek(8, 1)
         volume_resiprocal = np.abs(np.dot(np.cross(self.b1, self.b2), self.b3))
         self.omega = np.power(2 * np.pi, 3) / (volume_resiprocal)
-        self.a1 = (2 * np.pi * (np.cross(self.b2, self.b3) / volume_resiprocal)).tolist()
-        self.a2 = (2 * np.pi * (np.cross(self.b3, self.b1) / volume_resiprocal)).tolist()
-        self.a3 = (2 * np.pi * (np.cross(self.b1, self.b2) / volume_resiprocal)).tolist()
+        self.a1 = (
+            2 * np.pi * (np.cross(self.b2, self.b3) / volume_resiprocal)
+        ).tolist()
+        self.a2 = (
+            2 * np.pi * (np.cross(self.b3, self.b1) / volume_resiprocal)
+        ).tolist()
+        self.a3 = (
+            2 * np.pi * (np.cross(self.b1, self.b2) / volume_resiprocal)
+        ).tolist()
 
     @staticmethod
     def read_pdep(fname):
-        with open(fname, 'rb') as f:
-            header = np.fromfile(f, dtype='int32', count=32)
+        with open(fname, "rb") as f:
+            header = np.fromfile(f, dtype="int32", count=32)
             pdepg = np.fromfile(f, dtype=np.complex128, count=header[2])
         return pdepg
 
@@ -92,13 +123,14 @@ class qe_io(base_io):
         file_name, _ = os.path.splitext(base_name)
         return int(file_name.split("E")[-1])
 
-    def read_wstat(self,
-                   folder: str,
-                  ):
+    def read_wstat(
+        self,
+        folder: str,
+    ):
         fnames_all = os.listdir(folder)
         fnames = []
         for fname in fnames_all:
-            if '.dat' in fname:
+            if ".dat" in fname:
                 fnames.append(fname)
 
         # read pdepg
@@ -107,7 +139,7 @@ class qe_io(base_io):
         for idx, fname in zip(pdep_index, fnames):
             fnames_cp[idx - 1] = fname
         fnames = fnames_cp
-        self.pdepg = np.zeros( (len(fnames), len(self.mill)), dtype="complex128")
+        self.pdepg = np.zeros((len(fnames), len(self.mill)), dtype="complex128")
         fnames = [os.path.join(folder, fname) for fname in fnames]
         with ThreadPoolExecutor() as executor:
             results = executor.map(lambda fname: self.read_pdep(fname), fnames)
@@ -116,54 +148,46 @@ class qe_io(base_io):
 
         # read pdepeig
         # Load the output data
-        with open(os.path.join(folder, 'wstat.json')) as json_file :
+        with open(os.path.join(folder, "wstat.json")) as json_file:
             data = json.load(json_file)
 
         # Extract converged PDEP eigenvalues
-        self.pdepeig = np.array(data['exec']['davitr'][-1]['ev'],dtype='f8') # -1 identifies the last iteration
+        self.pdepeig = np.array(
+            data["exec"]["davitr"][-1]["ev"], dtype="f8"
+        )  # -1 identifies the last iteration
 
-
-    @staticmethod
-    def write_pdep(pdep_val: np.ndarray,
-                   fname: str):
-        header = np.zeros(HD_LENGTH, dtype='int32')
-        header[HD_ID_VERSION] = HD_VERSION
-        header[HD_ID_DIMENSION] = pdep_val.size
-        header[HD_ID_LITTLE_ENDIAN] = 1 if sys.byteorder == 'little' else 0
-        with open(fname, 'wb') as f:
-            f.write(header.tobytes())
-            f.write(pdep_val.astype(np.complex128).tobytes())
-
-    def write_summary(self,
-                      pdepeig: np.ndarray,
-                      pdep_fnames: List[str],
-                      fname: str='summary_westpy.json',
-                      ):
+    def write_summary(
+        self,
+        pdepeig: np.ndarray,
+        pdep_fnames: List[str],
+        fname: str = "summary_westpy.json",
+    ):
         data = {}
         data["dielectric_matrix"] = {}
-        data['dielectric_matrix']['domain'] = {}
-        data['dielectric_matrix']['domain']['a1'] = self.a1
-        data['dielectric_matrix']['domain']['a2'] = self.a2
-        data['dielectric_matrix']['domain']['a3'] = self.a3
-        data['dielectric_matrix']['domain']['b1'] = self.b1.tolist()
-        data['dielectric_matrix']['domain']['b2'] = self.b2.tolist()
-        data['dielectric_matrix']['domain']['b3'] = self.b3.tolist()
-        data['dielectric_matrix']['pdep'] = [{}]
-        data['dielectric_matrix']['pdep'][0]['iq'] = 1
-        data['dielectric_matrix']['pdep'][0]['q'] = [0.0, 0.0, 0.0]
-        data['dielectric_matrix']['pdep'][0]['eigenval'] = pdepeig.tolist()
-        data['dielectric_matrix']['pdep'][0]['eigenvec'] = pdep_fnames
-        with open(fname, 'w', encoding='utf-8') as f:
+        data["dielectric_matrix"]["domain"] = {}
+        data["dielectric_matrix"]["domain"]["a1"] = self.a1
+        data["dielectric_matrix"]["domain"]["a2"] = self.a2
+        data["dielectric_matrix"]["domain"]["a3"] = self.a3
+        data["dielectric_matrix"]["domain"]["b1"] = self.b1.tolist()
+        data["dielectric_matrix"]["domain"]["b2"] = self.b2.tolist()
+        data["dielectric_matrix"]["domain"]["b3"] = self.b3.tolist()
+        data["dielectric_matrix"]["pdep"] = [{}]
+        data["dielectric_matrix"]["pdep"][0]["iq"] = 1
+        data["dielectric_matrix"]["pdep"][0]["q"] = [0.0, 0.0, 0.0]
+        data["dielectric_matrix"]["pdep"][0]["eigenval"] = pdepeig.tolist()
+        data["dielectric_matrix"]["pdep"][0]["eigenvec"] = pdep_fnames
+        with open(fname, "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
 
-    def write_wstat(self,
-                    pdepeig: np.ndarray,
-                    pdepvec: np.ndarray,
-                    prefix: str = "westpy",
-                    eig_mat : str = 'chi',
-                    ):
-        assert eig_mat in ['chi', 'chi_0'], "eig_mat must be either 'chi' or 'chi_0'"
-        if eig_mat == 'chi':
+    def write_wstat(
+        self,
+        pdepeig: np.ndarray,
+        pdepvec: np.ndarray,
+        prefix: str = "westpy",
+        eig_mat: str = "chi",
+    ):
+        assert eig_mat in ["chi", "chi_0"], "eig_mat must be either 'chi' or 'chi_0'"
+        if eig_mat == "chi":
             pdepeig = pdepeig / (pdepeig + 1.0)
         outFolder = prefix + ".wstat.save"
         if not os.path.exists(outFolder):
@@ -174,10 +198,14 @@ class qe_io(base_io):
             fname = "Q" + str(1).zfill(9) + "E" + str(i + 1).zfill(9) + ".dat"
             fnames.append(fname)
 
-        def write_wstat_helper(vec, fname, outFolder):
-            self.write_pdep(vec, os.path.join(outFolder, fname))
+        with tempfile.TemporaryDirectory(dir=outFolder) as tempdir:
+            pdepvec_fname = os.path.join(tempdir, "pdepvec.npy")
+            np.save(pdepvec_fname, pdepvec)
 
-        with ThreadPoolExecutor() as executor:
-            executor.map(lambda vecfname: write_wstat_helper(*vecfname, outFolder=outFolder),
-                         zip(pdepvec, fnames))
-        self.write_summary(pdepeig, fnames, os.path.join(outFolder, 'summary.json'))
+            args = [
+                (i, fname, pdepvec_fname, outFolder) for i, fname in enumerate(fnames)
+            ]
+            with Pool() as pool:
+                pool.starmap(write_wstat_helper, args)
+
+        self.write_summary(pdepeig, fnames, os.path.join(outFolder, "summary.json"))
